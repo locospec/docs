359b77e6f7bbb33911aac86fe74bdd46bcc8ddef
Rajiv Seelam <rajiv@betalectic.com>
2024-10-22 13:42:18 +0530

    Add deploymentBranch

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -21,6 +21,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: "locospec", // Usually your GitHub org/user name.
   projectName: "locospec.github.io", // Usually your repo name.
+  deploymentBranch: "main",
   trailingSlash: false,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
