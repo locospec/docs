5d4c245b359161eb28878e2f4281ce1ce8bb06b3
Rajiv Seelam <rajiv@betalectic.com>
2024-12-26 16:55:21 +0530

    Add Tailwind CSS styles and update configuration for improved layout and design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,20 @@
 import { type Config } from "tailwindcss";
+import {
+  scopedPreflightStyles,
+  isolateInsideOfContainer,
+} from "tailwindcss-scoped-preflight";
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx"],
-  corePlugins: { preflight: false },
+  content: ["./src/**/*.html", "./src/**/*.js", "./src/**/*.tsx", "./*.ts"],
   darkMode: ["class", '[data-theme="dark"]'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  important: true,
+  plugins: [
+    require("@tailwindcss/typography"),
+    scopedPreflightStyles({
+      isolationStrategy: isolateInsideOfContainer(".twp", {
+        except: ".no-twp",
+      }),
+    }),
+  ],
 } satisfies Config;
